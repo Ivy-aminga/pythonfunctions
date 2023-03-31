eef437099438f49174743c0b06c74d8b795bff55
Ivy-aminga <amingaivy@gmail.com>
2023-03-31 21:44:51 +0300

    first commit

---
+++ functions/math_.py
@@ -0,0 +1,26 @@
+def add (a,b):
+    answer = (a+b)
+    return answer
+
+# subtract
+def subtract (z,x):
+    answer = (z-x)
+    return answer
+
+
+# divide
+def divide (v,n):
+    answer = (v/n)
+    return answer
+
+
+# multiply
+def multiply (k,m):
+    answer = (k*m)
+    return answer
+
+
+# remainder
+def remainder (p,t):
+    answer = (p%t)
+    return answer
